1f15007409209f70de1b52dfa85e8a1b717d758d
E.Musia <ewemusia@gmail.com>
2017-12-13 22:56:56 +0100

    2DBuilder scene added
    Reverting changes in main scene.
    Adding 2DBuilder scene with:
    + grid (1000 x 800)
    + buttons

---
+++ Assets/Scripts/BuilderController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class BuilderController : VRInteractableObject
+{
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject builder;
+
+    bool builderState = false;
+
+    void OnEnable()
+    {
+        controllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+    }
+
+    void OnDisable()
+    {
+        controllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoReleased;
+    }
+
+    private void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        builderState = !builderState;
+        builder.SetActive(builderState);
+    }
+}
