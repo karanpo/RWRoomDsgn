d61e50b6942bf70727d4850cab64be7626c7ae40
E.Musia <ewemusia@gmail.com>
2017-12-17 00:14:55 +0100

    Try to build a wall in choosen place
    Really ugly way.
    I need to refactor that but for now can be as it is.

---
+++ Assets/BuildWall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class BuildWall : VRTK_InterctableObject {
+
+    public PrimitiveTypes shape;
+    public bool resized = false;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+
+        if(!resized)
+        {
+            transform.localScale += new Vector3(0, 0.2F, 15F);
+            resized = true;
+        } else {
+            transform.localScale += new Vector3(0, -0.2F, -15F);
+            resized = false;
+        }
+        ResetMenuItems();
+    }
+
+    private void ResetMenuItems()
+    {
+        foreach (Menu_Object_Spawner menuObjectSpawner in FindObjectsOfType<Menu_Object_Spawner>())
+        {
+            menuObjectSpawner.StopUsing();
+        }
+    }
+
+}
---
+++ Assets/Scripts/ResizeObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeObject : MonoBehaviour {
+
+    Vector3 tempX, tempY;
+    public float sizingFactor = 0.02f;
+    private GameObject lastSpawn = null;
+    private float startSize;
+    private float startX;
+
+    if (Input.GetMouseButtonDown (0))
+    {
+             float positionZ = 10.0f;
+             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, positionZ);
+             startX = position.x;
+             position = Camera.main.ScreenToWorldPoint(position);
+             lastSpawn = Instantiate(cubePrefab, position, transform.rotation) as GameObject;
+             startSize = lastSpawn.transform.localScale.z;
+    }
+ 
+         if (Input.GetMouseButton (0)) {
+             Vector3 size = lastSpawn.transform.localScale;
+            size.x = startSize + (Input.mousePosition.x - startX) * sizingFactor;
+            lastSpawn.transform.localScale = size;
+         }
+     }
+
+    void ResizeX()
+    {
+        tempX = transform.localScale;
+        tempX.x += Time.deltaTime;
+        transform.localScale = tempX;
+    }
+
+    void Resize()
+    {
+        temp = transform.localScale;
+        temp.y += Time.deltaTime;
+        transform.localScale = tempY;
+    }
+
+}
